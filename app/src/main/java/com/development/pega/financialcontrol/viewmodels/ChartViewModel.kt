e8c372fbf77c9f4a9e30ea89d9e0d847c99e33de
PedroPSar <pedropsarmento@outlook.com>
2020-09-17 17:50:37 -0300

    Commit 17/09/2020

--- app/src/main/java/com/development/pega/financialcontrol/viewmodels/ChartViewModel.kt
+++ app/src/main/java/com/development/pega/financialcontrol/viewmodels/ChartViewModel.kt
@@ -1,7 +1,50 @@
 package com.development.pega.financialcontrol.viewmodels
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.development.pega.financialcontrol.R
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
-class ChartViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ChartViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mContext = application.applicationContext
+    private val months = mContext.resources.getStringArray(R.array.months_array)
+
+    private val mYearMonthsChart = MutableLiveData<LineData>()
+    var yearMonthsChart: LiveData<LineData> = mYearMonthsChart
+
+    fun setDataInYearMonthsLineChart() {
+
+        val balanceLine = LineDataSet(createYearMonthsArray(), mContext.getString(R.string.lbl_balance))
+        var dataSets = arrayListOf<ILineDataSet>()
+        dataSets.add(balanceLine)
+
+        val lineData = LineData(dataSets)
+        mYearMonthsChart.value = lineData
+    }
+
+    private fun createYearMonthsArray(): List<Entry> {
+        var data = arrayListOf<Entry>()
+        data.add(Entry(1f, 5f))
+        data.add(Entry(2f, 2f))
+        data.add(Entry(3f, 11f))
+        data.add(Entry(4f, 16f))
+        data.add(Entry(5f, 7f))
+        data.add(Entry(6f, 3f))
+        data.add(Entry(7f, 8f))
+        data.add(Entry(8f, 9f))
+        data.add(Entry(9f, 9f))
+        data.add(Entry(10f, 9f))
+        data.add(Entry(11f, 9f))
+        data.add(Entry(12f, 9f))
+
+        return data
+    }
 }
--- app/src/main/java/com/development/pega/financialcontrol/views/ChartFragment.kt
+++ app/src/main/java/com/development/pega/financialcontrol/views/ChartFragment.kt
@@ -2,18 +2,23 @@ package com.development.pega.financialcontrol.views
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.development.pega.financialcontrol.viewmodels.ChartViewModel
 import com.development.pega.financialcontrol.R
 import com.development.pega.financialcontrol.viewmodels.HomeViewModel
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
 class ChartFragment : Fragment() {
@@ -40,26 +45,33 @@ class ChartFragment : Fragment() {
 
         mYearMonthsLineChart = root.findViewById(R.id.line_chart_year_months)
 
-        val lineDataSet1 = LineDataSet(createArray(), "Months")
-        var dataSets = arrayListOf<ILineDataSet>()
-        dataSets.add(lineDataSet1)
-
-        val lineData = LineData(dataSets)
-        mYearMonthsLineChart.data = lineData
-        mYearMonthsLineChart.invalidate()
+        observers()
     }
 
-    private fun createArray(): List<Entry> {
-        var dataVals = arrayListOf<Entry>()
-        dataVals.add(Entry(1f, 5f))
-        dataVals.add(Entry(2f, 2f))
-        dataVals.add(Entry(3f, 11f))
-        dataVals.add(Entry(4f, 16f))
-        dataVals.add(Entry(5f, 7f))
-        dataVals.add(Entry(6f, 3f))
-        dataVals.add(Entry(7f, 8f))
+    override fun onResume() {
+        super.onResume()
 
-        return dataVals
+        viewModel.setDataInYearMonthsLineChart()
     }
 
+    private fun observers() {
+        viewModel.yearMonthsChart.observe(viewLifecycleOwner, Observer {
+            mYearMonthsLineChart.data = it
+            mYearMonthsLineChart.xAxis.valueFormatter = MonthsNamesFormatter()
+            mYearMonthsLineChart.invalidate()
+        })
+    }
+
+}
+
+class MonthsNamesFormatter() : ValueFormatter() {
+    private val days = arrayOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez")
+
+    override fun getFormattedValue(value: Float): String {
+        return value.toString()
+    }
+
+    override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+        return days.getOrNull(value.toInt()) ?: value.toString()
+    }
 }
