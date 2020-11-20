dff1063f6a5bc2e1c7e0194a2ea6fbe1688162f0
PedroPSar <pedropsarmento@outlook.com>
2020-11-20 00:45:36 -0300

    19/11/2020 Commit 2

--- app/src/main/java/com/development/pega/financialcontrol/viewmodels/SettingsViewModel.kt
+++ app/src/main/java/com/development/pega/financialcontrol/viewmodels/SettingsViewModel.kt
@@ -2,25 +2,10 @@ package com.development.pega.financialcontrol.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.development.pega.financialcontrol.R
-import com.development.pega.financialcontrol.service.repository.Prefs
 
 class SettingsViewModel(application: Application): AndroidViewModel(application) {
 
     private val mContext = application.applicationContext
-    private val currencies = mContext.resources.getStringArray(R.array.currencies)
 
-    private var mSelectedCurrency = MutableLiveData<String>()
-    var selectedCurrency: LiveData<String> = mSelectedCurrency
 
-    fun setSelectedCurrency(position: Int) {
-        mSelectedCurrency.value = currencies[position]
-    }
-
-    fun saveSelectedCurrency(value: String) {
-        val prefs = Prefs(mContext)
-        prefs.currencySelectedValue = value
-    }
 }
--- app/src/main/java/com/development/pega/financialcontrol/views/SettingsActivity.kt
+++ app/src/main/java/com/development/pega/financialcontrol/views/SettingsActivity.kt
@@ -1,24 +1,27 @@
 package com.development.pega.financialcontrol.views
 
+import android.content.DialogInterface
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.widget.Toolbar
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.development.pega.financialcontrol.R
 import com.development.pega.financialcontrol.viewmodels.SettingsViewModel
+import com.skydoves.colorpickerview.ColorEnvelope
+import com.skydoves.colorpickerview.ColorPickerDialog
+import com.skydoves.colorpickerview.flag.BubbleFlag
+import com.skydoves.colorpickerview.flag.FlagMode
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
+import kotlinx.android.synthetic.main.activity_settings.*
 
-class SettingsActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class SettingsActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mSettingsViewModel: SettingsViewModel
     private lateinit var mViewModelFactory: ViewModelProvider.AndroidViewModelFactory
-
     private lateinit var mToolbar: Toolbar
-    private lateinit var mCurrencySpinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,38 +29,40 @@ class SettingsActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
 
         setViewModel()
         setToolbarOptions()
-        setSpinners()
         observers()
         setListeners()
     }
 
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        if(parent?.id == R.id.spinner_currency) {
-            mSettingsViewModel.setSelectedCurrency(position)
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.view_incomes_color -> showColorDialog(view_incomes_color)
+            R.id.view_expenses_color -> showColorDialog(view_expenses_color)
+            R.id.view_balance_color -> showColorDialog(view_balance_color)
+            R.id.view_not_required_color -> showColorDialog(view_not_required_color)
+            R.id.view_required_color -> showColorDialog(view_required_color)
+            R.id.view_investment_color -> showColorDialog(view_investment_color)
+            R.id.view_expenses_installment_color -> showColorDialog(view_expenses_installment_color)
+            R.id.view_expenses_fixed_monthly_color -> showColorDialog(view_expenses_fixed_monthly_color)
+            R.id.view_incomes_installment_color -> showColorDialog(view_incomes_installment_color)
+            R.id.view_incomes_fixed_monthly_color -> showColorDialog(view_incomes_fixed_monthly_color)
         }
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        TODO("Not yet implemented")
     }
 
     private fun setListeners() {
-        mCurrencySpinner.onItemSelectedListener = this
-    }
-
-    private fun setSpinners() {
-        mCurrencySpinner = findViewById(R.id.spinner_currency)
-        ArrayAdapter.createFromResource(this, R.array.currencies, android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            mCurrencySpinner.adapter = adapter
-        }
+        view_incomes_color.setOnClickListener(this)
+        view_expenses_color.setOnClickListener(this)
+        view_balance_color.setOnClickListener(this)
+        view_not_required_color.setOnClickListener(this)
+        view_required_color.setOnClickListener(this)
+        view_investment_color.setOnClickListener(this)
+        view_expenses_installment_color.setOnClickListener(this)
+        view_expenses_fixed_monthly_color.setOnClickListener(this)
+        view_incomes_installment_color.setOnClickListener(this)
+        view_incomes_fixed_monthly_color.setOnClickListener(this)
     }
 
     private fun observers() {
-        mSettingsViewModel.selectedCurrency.observe(this, Observer {
-            mSettingsViewModel.saveSelectedCurrency(it)
-        })
+
     }
 
     private fun setViewModel() {
@@ -73,6 +78,40 @@ class SettingsActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
         supportActionBar?.title = getString(R.string.settings_title)
 
         mToolbar.setNavigationOnClickListener{ onBackPressed() }
+    }
+
+    fun showColorDialog(view: View) {
+        val colorDrawable = view.background as ColorDrawable
+        val viewColor = colorDrawable.color
+
+        val bubbleFlag = BubbleFlag(this)
+        bubbleFlag.flagMode = FlagMode.ALWAYS
+
+        val colorBuilder = ColorPickerDialog.Builder(this)
+            .setTitle(getString(R.string.color_dialog_title))
+            .setPreferenceName(getString(R.string.color_dialog_title))
+            .setPositiveButton(this.getString(R.string.color_dialog_positive_button), object:
+                ColorEnvelopeListener {
+                override fun onColorSelected(envelope: ColorEnvelope?, fromUser: Boolean) {
+                    if (envelope != null) {
+                        view.setBackgroundColor(envelope.color)
+                    }
+                }
+            })
+            .setNegativeButton(this.getString(R.string.color_dialog_negative_button), object:
+                DialogInterface.OnClickListener{
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    dialog?.dismiss()
+                }
+            })
+            .attachAlphaSlideBar(false)
+            .attachBrightnessSlideBar(false)
+            .setBottomSpace(12)
+
+        val colorPickerView = colorBuilder.colorPickerView
+        colorPickerView.flagView = bubbleFlag
+        colorPickerView.setInitialColor(resources.getColor(R.color.investment_color))
+        colorBuilder.show()
     }
 
 }
